079c70c97b661c125bdaaec492b5560dfb431ab7
N.Takuya <nishiinfo0408@gmail.com>
2025-10-30 15:17:55 +0900

    書き込み処理までを追加

--- src/lecture05_01.py
+++ src/lecture05_01.py
@@ -8,10 +8,11 @@ def lecture05_01():
     app = MyVideoCapture()
     app.run()
 
+    app.get_img()
+
     # 画像をローカル変数に保存
     google_img : cv2.Mat = cv2.imread('images/google.png')
-    capture_img : cv2.Mat = cv2.imread('images/camera_capture.png') # 動作テスト用なので提出時にこの行を消すこと
-    # capture_img : cv2.Mat = "implement me"
+    capture_img : cv2.Mat = app.captured_img
 
     g_hight, g_width, g_channel = google_img.shape
     c_hight, c_width, c_channel = capture_img.shape
@@ -23,9 +24,7 @@ def lecture05_01():
             g, b, r = google_img[y, x]
             # もし白色(255,255,255)だったら置き換える
             if (b, g, r) == (255, 255, 255):
-                pass
-                #implement me
-
+                # google_imgの方が大きいので、適切な場所に配置できるように処理する
+                google_img[y, x] = capture_img[y%capture_img.shape[0], x%capture_img.shape[1]]
     # 書き込み処理
-    # implement me
-
+    cv2.imwrite("images/edited_google_img.png",google_img)
